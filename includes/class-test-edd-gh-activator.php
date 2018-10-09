f2a38da010d9fcf6085759b4fc41d7e6de7b14b2
theritesite <theritesites@gmail.com>
2018-10-08 20:20:06 -0700

    first commit

---
+++ includes/class-test-edd-gh-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.theritesites.com
+ * @since      1.0.0
+ *
+ * @package    Test_EDD_GH
+ * @subpackage Test_EDD_GH/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Test_EDD_GH
+ * @subpackage Test_EDD_GH/includes
+ * @author     TheRiteSites <contact@theritesites.com>
+ */
+class Test_EDD_GH_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-test-edd-gh-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.theritesites.com
+ * @since      1.0.0
+ *
+ * @package    Test_EDD_GH
+ * @subpackage Test_EDD_GH/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Test_EDD_GH
+ * @subpackage Test_EDD_GH/includes
+ * @author     TheRiteSites <contact@theritesites.com>
+ */
+class Test_EDD_GH_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
