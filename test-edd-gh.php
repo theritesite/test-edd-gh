c319ac5b83ca851a26db8ac9b0722f4cbb491435
Parker Mathewson <theritesites@gmail.com>
2018-10-16 16:25:53 -0700

    v1.0.12.1

--- test-edd-gh.php
+++ test-edd-gh.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Test EDD Github Updater
  * Plugin URI:        https://www.theritesites.com/plugins/test-edd-gh
  * Description:       This plugin does awesome things!
- * Version:           1.0.12
+ * Version:           1.0.12.1
  * Author:            TheRiteSites
  * Author URI:        https://www.theritesites.com
  * License:           GPL-2.0+
@@ -33,7 +33,7 @@ if ( ! defined( 'WPINC' ) ) {
 /**
  * Current plugin version.
  */
-define( 'TEST_EDD_GH_VERSION', '1.0.12' );
+define( 'TEST_EDD_GH_VERSION', '1.0.12.1' );
 
 define( 'TEST_EDD_GH_UPDATER_URL', 'https://test-theritesites.pantheonsite.io' );
 
